59bcf29ca719ef5cff69e50ab77f7fcbb6f5efb8
Mihir Bhatkar <mihirbhatkar87@gmail.com>
2023-10-06 00:06:05 +0530

    some progress?

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,104 @@
-let matrix = [
-	[1, 0],
-	[0, 0],
-];
+const side = 4;
 
-let currentPosition = {
-	x: 0,
-	y: 0,
+let snake = {
+	points: [
+		{ x: 0, y: 0 }, // first element will be tail
+		{ x: 1, y: 0 },
+		{ x: 2, y: 0 }, // last element will be head
+	],
+	length: 3,
+	direction: "right",
 };
 
-// let universalDirection = "right";
+const newPoints = (points, direction) => {
+	const newPoints = structuredClone(points);
+	let newHead = newPoints.slice(-1)[0];
+
+	let newX = newHead.x;
+	let newY = newHead.y;
+	switch (direction) {
+		case "right":
+			newPoints.push({ x: ++newX, y: newY });
+			break;
+		case "left":
+			newPoints.push({ x: --newX, y: newY });
+			break;
+		case "up":
+			newPoints.push({ x: newX, y: --newY });
+			break;
+		case "down":
+			newPoints.push({ x: newX, y: ++newY });
+			break;
+	}
+	newPoints.shift();
+	return newPoints;
+};
 
 const checkKey = (e) => {
-	const currentPositionOld = JSON.parse(JSON.stringify(currentPosition));
+	const currentSnake = JSON.parse(JSON.stringify(snake));
+	const pointsOld = currentSnake.points;
+
+	let nextSnake = "";
 	switch (e.key) {
 		case "ArrowRight":
-			return { x: ++currentPositionOld.x, y: currentPositionOld.y };
+			nextSnake = {
+				...currentSnake,
+				points: newPoints(pointsOld, "right"),
+			};
+			break;
 		case "ArrowLeft":
-			return { x: --currentPositionOld.x, y: currentPositionOld.y };
+			nextSnake = {
+				...currentSnake,
+				points: newPoints(pointsOld, "left"),
+			};
+			break;
 		case "ArrowUp":
-			return { x: currentPositionOld.x, y: --currentPositionOld.y };
+			nextSnake = {
+				...currentSnake,
+				points: newPoints(pointsOld, "up"),
+			};
+			break;
 		case "ArrowDown":
-			return { x: currentPositionOld.x, y: ++currentPositionOld.y };
+			nextSnake = {
+				...currentSnake,
+				points: newPoints(pointsOld, "down"),
+			};
+			break;
 		default:
 			return undefined;
 	}
+	if (checkIfStepExists(nextSnake)) {
+		return nextSnake;
+	} else return undefined;
 };
 
-// const stepExistence = () => {};
+const checkIfStepExists = (nextSnake) => {
+	const head = nextSnake.points.slice(-1)[0];
+	console.log(head.x, head.y);
+	return !(
+		head.x < 0 ||
+		head.y < 0 ||
+		head.x > side - 1 ||
+		head.y > side - 1
+	);
+};
+
+const drawSnake = (newSnake) => {
+	const selected = document.querySelectorAll(".selected");
+	for (let item of selected) item.removeAttribute("class"); //  remove current snake
+
+	const newSelectedIds = [];
+	for (const item of newSnake.points)
+		newSelectedIds.push(`${item.x} ${item.y}`);
+
+	for (const id of newSelectedIds)
+		document.getElementById(id).setAttribute("class", "selected");
+};
 
 const walk = (e) => {
-	const newSquare = checkKey(e);
-	if (!newSquare) return; // don't do anything if other keys are pressed
-
-	// check if step exists
-	if (
-		newSquare.x < 0 ||
-		newSquare.y < 0 ||
-		newSquare.x > 1 ||
-		newSquare.y > 1
-	) {
-		// console.error(currentPosition);
-		// console.error(newSquare);
-		console.error("step doesn't exist!");
-	} else {
-		// console.log(newSquare);
-		// console.info("exists!!");
-
-		const selected = document.querySelector(".selected");
-		const [x, y] = selected.id.split(" ").map(Number); // getting x and y coordinates
-
-		selected.removeAttribute("class");
-
-		currentPosition = { x: newSquare.x, y: newSquare.y };
-		// console.info("the position after moving is ", currentPosition);
-
-		const newSelectedId = `${currentPosition.x} ${currentPosition.y}`;
-		document
-			.getElementById(newSelectedId)
-			.setAttribute("class", "selected");
-	}
+	const newSnake = checkKey(e);
+	if (!newSnake) return; // don't do anything if other keys are pressed
+	drawSnake(newSnake);
 };
 
 window.addEventListener("keydown", walk);
